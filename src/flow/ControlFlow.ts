5a827ac04def7d6b0416536e53604e18733598ce
DoTheSimplest <249650346+DoTheSimplest@users.noreply.github.com>
2025-12-15 15:40:14 +0900

    refactor: make base class of control flow abstract

--- src/flow/ControlFlow.ts
+++ src/flow/ControlFlow.ts
@@ -1,5 +1,7 @@
-export class ControlFlow {
+export abstract class ControlFlow {
 	prev: ControlFlow | Node | null = null;
 	next: ControlFlow | Node | null = null;
 	nodes = [] as Node[];
+
+	abstract run(element: Element): void;
 }
