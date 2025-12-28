5f087caaf7e4e0c4bb98e18cbd24baee9c48c54c
DoTheSimplest <249650346+DoTheSimplest@users.noreply.github.com>
2025-12-29 02:17:29 +0900

    test: add `text` after animation finished

--- test/examples/animate.test.ts
+++ test/examples/animate.test.ts
@@ -3,12 +3,12 @@ import { div, Modify } from "../../src";
 
 describe("animate", () => {
 	it("simple duration", () => {
-		//
 		Modify(document.body, { html: "" }, [
 			div(
 				"Hello!",
 				{ css: { background: "blue" } },
 				{ css: { background: "lime" }, animate: 1000 },
+				{ text: "Animation Finished" },
 			),
 		]);
 	});
